9255c3995408983bde5a11529b68cb403d4a3643
Danial Takdehghan <98669366+DanielDehghan@users.noreply.github.com>
2024-10-11 13:04:12 -0400

    Adds Models and views

--- ConestogaVirtualGameStore/ViewModels/LoginViewModel.cs
+++ ConestogaVirtualGameStore/ViewModels/LoginViewModel.cs
@@ -1,6 +1,23 @@
-﻿namespace ConestogaVirtualGameStore.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConestogaVirtualGameStore.ViewModels
 {
     public class LoginViewModel
     {
+       
+        [Required(ErrorMessage = "Email is Invalid")]
+        [EmailAddress]
+        [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$", ErrorMessage = "Email is invalid should contain @")]
+
+        public string Email { get; set; }
+
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(100, ErrorMessage = "Password \"{0}\" must have {2} characters", MinimumLength = 8)]
+        [RegularExpression(@"^([a-zA-Z0-9@*#]{8,15})$", ErrorMessage = "Password must contain: Minimum 8 characters, at least 1 UpperCase Alphabet, 1 LowerCase Alphabet, 1 Number, and 1 Special Character")]
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
     }
 }
--- ConestogaVirtualGameStore/ViewModels/RegisterViewModel.cs
+++ ConestogaVirtualGameStore/ViewModels/RegisterViewModel.cs
@@ -1,6 +1,29 @@
-﻿namespace ConestogaVirtualGameStore.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConestogaVirtualGameStore.ViewModels
 {
     public class RegisterViewModel
     {
+        [Required(ErrorMessage = "Please enter first name")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Please enter last name")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email is Invalid")]
+        [EmailAddress]
+        [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$", ErrorMessage = "Email is invalid should contain @")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(100, ErrorMessage = "Password \"{0}\" must have {2} characters", MinimumLength = 8)]
+        [RegularExpression(@"^([a-zA-Z0-9@*#]{8,15})$", ErrorMessage = "Password must contain: Minimum 8 characters, at least 1 UpperCase Alphabet, 1 LowerCase Alphabet, 1 Number, and 1 Special Character")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Please enter confirm password")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Confirm password doesn't match, Type again!")]
+        public string ConfirmPassword { get; set; }
+
     }
 }
