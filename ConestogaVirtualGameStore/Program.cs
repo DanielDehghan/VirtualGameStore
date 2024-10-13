dc3602f6e366ed6a3bc2a4967ecb9f2194ce9b34
Danial Takdehghan <98669366+DanielDehghan@users.noreply.github.com>
2024-10-13 03:13:32 -0400

    adds Roles to the service

--- ConestogaVirtualGameStore/Program.cs
+++ ConestogaVirtualGameStore/Program.cs
@@ -26,6 +26,15 @@ builder.Services.ConfigureApplicationCookie(options =>
 
 var app = builder.Build();
 
+// Create roles and admin user at application startup
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+    var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+
+    await CreateRoles(roleManager, userManager);
+}
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
@@ -50,3 +59,37 @@ app.Run();
 
 
 
+async Task CreateRoles(RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager)
+{
+	String[] roleNames = { "Admin", "Member" };
+	IdentityResult roleResult;
+
+	foreach (var roleName in roleNames)
+	{
+		var roleExist = await roleManager.RoleExistsAsync(roleName);
+		if (!roleExist)
+		{
+			roleResult = await roleManager.CreateAsync(new IdentityRole(roleName));
+		}
+	}
+
+	var adminUser = new ApplicationUser
+	{
+		UserName = "admin@game.com",
+		Email = "admin@game",
+		FirstName = "Admin",
+		LastName = "User"
+	};
+
+	var userPassword = "Admin@123";
+	var user = await userManager.FindByEmailAsync("admin@game.com");
+
+	if(user == null)
+	{
+		var createPowerUser = await userManager.CreateAsync(adminUser, userPassword);
+		if (createPowerUser.Succeeded)
+		{
+			await userManager.AddToRoleAsync(adminUser, "Admin");
+		}
+	}
+}
