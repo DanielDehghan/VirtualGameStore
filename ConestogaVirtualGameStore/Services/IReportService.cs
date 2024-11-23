50598b2cf38352fd1134e097a2b967255dbf29c0
Danial Takdehghan <98669366+DanielDehghan@users.noreply.github.com>
2024-11-23 01:28:55 -0500

    Adds Reports Interface

---
+++ ConestogaVirtualGameStore/Services/IReportService.cs
@@ -0,0 +1,9 @@
+﻿namespace ConestogaVirtualGameStore.Services
+{
+    public interface IReportService
+    {
+        Task<byte[]> GenerateReportAsync(int reportId, string format);
+        byte[] GenerateExcelReport(IEnumerable<object> data);
+        byte[] GeneratePdfReport(IEnumerable<object> data);
+    }
+}
